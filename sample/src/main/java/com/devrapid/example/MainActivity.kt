832e81e92d0b77949ed450c313408de1151ab815
Jieyi <pokkbaby@gmail.com>
2018-02-07 01:22:56 +0900

    Added the cache for gradle.

--- sample/src/main/java/com/devrapid/example/MainActivity.kt
+++ sample/src/main/java/com/devrapid/example/MainActivity.kt
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.btn_1
 import kotlinx.android.synthetic.main.activity_main.btn_click
 import kotlinx.android.synthetic.main.activity_main.btn_else
-import kotlinx.android.synthetic.main.activity_main.ttt
 
 class MainActivity : AppCompatActivity() {
     var time = 100
@@ -13,31 +12,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ttt.apply {
-            //            playAnimator(5)
-            totalTime = 10
-            onProgressChanged = { progress, remainedTime ->
-                //                logw(progress, remainedTime)
-            }
-            progress = 10.0
-        }
-
-//        rotatedCircleImageView.start()
-//
 //        rotatedCircleImageView.onClickEvent = { view, isPaused ->
 //        }
 //        rotatedCircleImageView.onChangeTime = { _, _ ->
 //        }
         btn_click.setOnClickListener {
-            ttt.stopAnimator()
         }
         btn_else.setOnClickListener {
-            ttt.playAnimator()
         }
         btn_1.setOnClickListener {
-            //            ttt.progress = 30.0
-
-            ttt.currentTime = 9
         }
     }
 }
