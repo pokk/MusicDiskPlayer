8a6ae10c55e574b545efc662532ed2798ea75c72
Jieyi <pokkbaby@gmail.com>
2018-02-19 16:28:18 +0900

    Upgraded the lib.

--- sample/src/main/java/com/devrapid/example/MainActivity.kt
+++ sample/src/main/java/com/devrapid/example/MainActivity.kt
@@ -5,12 +5,15 @@ import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.btn_1
 import kotlinx.android.synthetic.main.activity_main.btn_click
 import kotlinx.android.synthetic.main.activity_main.btn_else
+import kotlinx.android.synthetic.main.activity_main.rcii
 
 class MainActivity : AppCompatActivity() {
     var time = 100
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rcii.currProgress = 0.4f
 
 //        rotatedCircleImageView.onClickEvent = { view, isPaused ->
 //        }
